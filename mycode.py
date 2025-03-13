6bd7e0304122a957ce988c5a4d139206254a9174
vsairaamresearch <vsairaamresearch@gmail.com>
2025-03-13 17:52:42 +0530

    added data

--- mycode.py
+++ mycode.py
@@ -3,9 +3,9 @@ import os
 
 data = {"Name" : ["swami", "baba"], "age" : [10, 12], "city" : ["x", "y"]}
 df = pd.DataFrame(data)
-# # Adding new row to df for V2
-# new_row_loc = {'Name': 'GF1', 'Age': 20, 'City': 'City1'}
-# df.loc[len(df.index)] = new_row_loc
+# Adding new row to df for V2
+new_row_loc = {'Name': 'GF1', 'Age': 20, 'City': 'City1'}
+df.loc[len(df.index)] = new_row_loc
 
 # # # Adding new row to df for V3
 # new_row_loc2 = {'Name': 'GF2', 'Age': 30, 'City': 'City2'}
